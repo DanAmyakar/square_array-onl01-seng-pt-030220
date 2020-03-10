506fccee43f6d3847e9f1d9784ec550b137cec6e
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-10 21:49:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
   # your code here
-  array.each{ |rndm_num| new_nums = rndm_num ** 2
-  new_squares = [ ]
-  new_squares << new_nums
+  array.each{ |rndm_num| rndm_num ** 2}
+
 end
 
+
+
+def new_square_array(array)
+  array.each{|rndm_num| }
+    new_squares = [ ]
+  new_squares << new_nums
