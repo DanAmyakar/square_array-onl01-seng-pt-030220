f720ac97f67ccbe6e38e59e146edbd53fa86c69a
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-10 22:05:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,19 +1,4 @@
 def square_array(array)
-  array.each{ |num| puts #{num} ** 2}
-end
-
-print square_array
-
-
-new_square_array = [ ]
-
-def new_square_array(array)
-
-  array.each{|rndm_num| new_square_array << rndm_num ** 2}
-  return new_square_array
-
-end
-def square_array(array)
   new_array = []
   
   array.each{ |rndm_num| new_array << rndm_num ** 2}
