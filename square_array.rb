2a8a6708a652b7b64deffeb11a2618e822c1acde
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-10 21:58:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   array.each{|rndm_num| rndm_num ** 2}
 end
 
-new_square_array = [ ]
+#new_square_array = [ ]
 
-def new_square_array(array)
-  array.each{|rndm_num| new_square_array << rndm_num ** 2}
-end
+#def new_square_array(array)
+#  array.each{|rndm_num| new_square_array << rndm_num ** 2}
+#end
