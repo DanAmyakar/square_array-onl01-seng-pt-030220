3c41ef76d945d3d333e730746769d2ba8bb9c2be
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-10 21:28:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,5 @@
 def square_array(array)
   # your code here
-  array.each{ |rndm_num| puts rndm_num ** 2}
-  
-end
-
-
-def new_square_array(square_array)
-  
-  square_array.collect{|rndm_nums| rndm_nums ** 2}
+  array.collect{ |rndm_num| puts rndm_num ** 2}
   
 end
